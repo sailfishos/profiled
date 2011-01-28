f95f341ec8f5128289067011d684ee63e51de378
Narasimha Reddy Bommu <ext-narasimha.bommu@nokia.com>
2011-01-28 16:49:08 +0200

    formatted copyright text

--- depend_filter.py
+++ depend_filter.py
@@ -1,23 +1,38 @@
 #! /usr/bin/env python
 
 # =============================================================================
-# File: depend_filter.py
+# This file is part of profile-qt
 #
-# Copyright (C) 2007 Nokia. All rights reserved.
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+# All rights reserved.
 #
-# Author: Simo Piiroinen <simo.piiroinen@nokia.com>
+# Contact: Sakari Poussa <sakari.poussa@nokia.com>
 #
-# -----------------------------------------------------------------------------
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# History:
+# Redistributions of source code must retain the above copyright notice, 
+# this list of conditions and the following disclaimer. Redistributions in 
+# binary form must reproduce the above copyright notice, this list of 
+# conditions and the following disclaimer in the documentation  and/or 
+# other materials provided with the distribution.
 #
-# 05-Dec-2007 Simo Piiroinen
-# - initial version
+# Neither the name of Nokia Corporation nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+# OF THE POSSIBILITY OF SUCH DAMAGE.
 # =============================================================================
-
-# gcc -MM filters out only standard includes, which
-# does not cover glib etc headers ... so we filter
-# out all dependencies with absolute path
 
 import sys,os
 
